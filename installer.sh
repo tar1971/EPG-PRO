8b5af1e317f184a93289ca6929dbdb7512ddac5e
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-09 02:36:06 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@
 #
 ###########################################
 wget -O /tmp/EPG-PRO-grabber.tar "https://github.com/tar1971/EPG-PRO/blob/main/EPG-PRO-grabber.tar.gz?raw=true"
-###########################################
+echo ########################################
 tar -xzf /tmp/*.tar.gz -C /
 ###########################################
 wait
